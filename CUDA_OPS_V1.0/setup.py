23aac4d571ae1ff776b3ddcf6e43c385bb59b9fb
Bohlsen <79351569+Bohlsen@users.noreply.github.com>
2021-05-28 13:23:10 +1000

    Add files via upload

---
+++ CUDA_OPS_V1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+   name='CUDA_OPS',
+   version='1.0',
+   author='Nicholas Bohlsen',
+   author_email='bohlsennicholas@gmail.com',
+   packages=['CUDA_OPS'],
+)
